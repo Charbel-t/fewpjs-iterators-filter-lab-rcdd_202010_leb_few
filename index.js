627bf41940d805092ab05c83898c758175b5b55a
Charbel t <charbel2000.52@gmail.com>
2020-12-07 13:11:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(list,name){
+  return (list.filter(function(drivername){
+    return drivername.toLowerCase()===name.toLowerCase()}));
+}
+
+function fuzzyMatch(list, name){
+  let l=name.length;
+  return list.filter(function(drivername){
+    return drivername.slice(0,l)===name;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
